504ed6491888df8c054a1c7e2693ffdde310c288
bcherrera14 <bcherrera14@gmail.com>
2021-01-25 07:17:14 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	componentDidMount() {
+		const baseURL = 'https://swapi.dev/api/';
+
+		axios
+			.get(baseURL + 'planets/1/')
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
+	render() {
+		return (
+			<div className="container d-flex justify-content-center">
+				<h1>Star Wars App</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
